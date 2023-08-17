cc725e9c1d0919348f6a152dc317d6651dc64080
Rialahmed0 <rashedahommod0@gmail.com>
2023-08-17 09:53:36 +0600

    html css js

--- to do list/script.js
+++ to do list/script.js
@@ -16,4 +16,13 @@ document.getElementById("add-task").addEventListener('click', function () {
 
         inputBox.value = '';
     }
+
+    listContainer.addEventListener('click', function (e) {
+        if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('checked');
+        }
+        else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove()
+        }
+    }, false);
 })
